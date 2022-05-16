1e13a7261506adb10f746672148d6d8b0c93ef7a
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-16 18:33:13 +0300

    Import Browser Router component from react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
