9328100a4eda2ab496cbbdd9a54ee70ba3d0f1cd
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-18 00:54:57 +0300

    Add continents reducer

--- src/redux/continents/continents.js
+++ src/redux/continents/continents.js
@@ -1,3 +1,6 @@
+import { FaLastfmSquare } from "react-icons/fa";
+import continentsInitialData from "./initial-data";
+
 // Actions 
 const GET_CONTINENTS_SUCCESS = '/covid-tracker/GET_CONTINENTS_SUCCESS';
 const GET_CONTINENTS_LOADING = '/covid-tracker/GET_CONTINENTS_LOADING';
@@ -14,3 +17,29 @@ export const continentsFailed = (msg) => ({
   type: GET_CONTINENTS_FAILED,
   payload: msg
 });
+
+// continents reducer
+
+export default continents = (state = continentsInitialData, action) => {
+  switch(action.type) {
+    case GET_CONTINENTS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_CONTINENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        continents: action.payload,        
+      };
+    case GET_CONTINENTS_FAILED:
+      return {
+        ...state,
+        loading: false,
+        errMsg: action.payload
+      };
+    default:
+      return state;
+  }
+};
