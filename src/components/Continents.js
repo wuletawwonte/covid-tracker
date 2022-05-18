27e3426f6d4679ae96667f27d992a516bc544743
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-18 11:46:17 +0300

    Display name of continents by fetching from redux store

--- src/components/Continents.js
+++ src/components/Continents.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContinents } from '../redux/continents/continents';
 
 const Continents = () => {
   const continents = useSelector((state) => state.continents.continents);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContinents());
+  }, []);
 
   console.log('continents: ', continents);
 
