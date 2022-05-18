70fb4b8d13d4e9c31dbfa96230e63f2b2dff5c0b
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-18 17:51:32 +0300

    Add Global death stat in the homepage by fetching data from the API

---
+++ src/components/WorldWidget.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import style from './WorldWidget.module.css';
+
+const WorldWidget = () => {
+  const [deathData, setDeathData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://corona.lmao.ninja/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setDeathData(data.deaths);
+      })
+      .catch((err) => {
+        setDeathData(err.message);
+      });
+  }, []);
+
+  return (
+    <div className={style.worldStatsContianer}>
+      <img src="images/World.png" alt="World map" className={style.worldMap} />
+      <div className={style.worldStats}>
+        <h3>World Widget</h3>
+        {deathData ? (
+          <h4>
+            {`${deathData} `}
+            Deaths
+          </h4>
+        ) : <h4>Loading...</h4>}
+      </div>
+    </div>
+  );
+};
+
+export default WorldWidget;
