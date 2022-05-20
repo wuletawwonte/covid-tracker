ab79031e66d926ade1200f8db4de83d96ce4aa66
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-20 11:36:26 +0300

    Fix proptype missmatch error

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from '../components/Navbar';
+import { useParams } from 'react-router-dom';
+import CountriesNavbar from '../components/CountriesNavbar';
 
 const CountryPage = () => {
+  const { country } = useParams();
+  const [countryDetails, setCountryDetails] = useState({});
+
+  useEffect(() => {
+    fetch(`https://corona.lmao.ninja/v3/covid-19/countries/${country}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryDetails(data);
+      });
+  });
 
   return (
     <>
-      <Navbar />
+      <CountriesNavbar />
       <h2>Country page</h2>
+      <h2>{country}</h2>
+      <h2>{JSON.stringify(countryDetails)}</h2>
     </>
   );
-}
+};
 
 export default CountryPage;
