d7dc690cd6bbcd3cf36523ee422107e407da8dae
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-20 10:50:28 +0300

    Add corona background picture

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
-const CountryPage = () => (
-  <>
-    <Navbar />
-    <h2>Country page</h2>
-  </>
-);
+const CountryPage = () => {
+
+  return (
+    <>
+      <Navbar />
+      <h2>Country page</h2>
+    </>
+  );
+}
 
 export default CountryPage;
