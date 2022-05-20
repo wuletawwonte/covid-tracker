498b4515af45b7ffe05d9a7a7ab3379d20d1d620
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-20 12:17:02 +0300

    Add details section for a country

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -5,7 +5,7 @@ import style from './CountryPage.module.css';
 
 const CountryPage = () => {
   const { country } = useParams();
-  const [countryDetails, setCountryDetails] = useState({});
+  const [countryDetails, setCountryDetails] = useState(null);
 
   useEffect(() => {
     fetch(`https://corona.lmao.ninja/v3/covid-19/countries/${country}`)
@@ -18,11 +18,40 @@ const CountryPage = () => {
   return (
     <>
       <SubpageNavbar />
-      <div className={style.countryHeader}>
-        <h2>Country page</h2>
-      </div>
-      <h2>{country}</h2>
-      <h2>{JSON.stringify(countryDetails)}</h2>
+      {countryDetails
+        ? (
+          <>
+            <div className={style.countryHeader}>
+              <div className={style.countryFlagName}>
+                <img src={countryDetails.countryInfo.flag} alt={countryDetails.country} />
+                <h2>{countryDetails.country}</h2>
+              </div>
+            </div>
+            <div>
+              <div className={style.detailItem}>
+                <h3>Cases</h3>
+                <h3>{countryDetails.cases}</h3>
+              </div>
+              <div className={style.detailItem}>
+                <h3>Recovered</h3>
+                <h3>{countryDetails.recovered}</h3>
+              </div>
+              <div className={style.detailItem}>
+                <h3>Active</h3>
+                <h3>{countryDetails.active}</h3>
+              </div>
+              <div className={style.detailItem}>
+                <h3>Critical</h3>
+                <h3>{countryDetails.critical}</h3>
+              </div>
+              <div className={style.detailItem}>
+                <h3>Tests</h3>
+                <h3>{countryDetails.tests}</h3>
+              </div>
+            </div>
+          </>
+        )
+        : <h2>Loading...</h2>}
     </>
   );
 };
