daf88e6edf31ab4f770cd3c060062fa16ca5f598
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-22 23:45:42 +0300

    Add styling for the Theme button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { FaBars, FaRegSun } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { FaBars, FaAdjust } from 'react-icons/fa';
 import style from './Navbar.module.css';
 
 export default function Navbar() {
+  const theme = useSelector((state) => state.themes.theme);
+  const myStyle = {
+    backgroundColor: theme.navbarBg,
+  };
+
   return (
     <>
-      <header className={style.header}>
+      <header className={style.header} style={myStyle}>
         <FaBars className={style.nbIcon} />
         <h1>Covid Tracker</h1>
         <div className={style.navbarRight}>
-          <FaRegSun className={style.nbIcon} />
+          <FaAdjust className={style.nbIconWithText} />
+          <h3>Theme</h3>
         </div>
       </header>
     </>
