585eb36a11c505e44d5d1310372551c665e23afe
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-22 22:28:14 +0300

    Add two color themes

---
+++ src/redux/themes/initial-theme-data.js
@@ -0,0 +1,18 @@
+const themes = [
+  {
+    title: 'Blue',
+    navbarBg: '#4369b2',
+    heroBg: '#5585e1',
+    headerBg: '#325083',
+    widgetBgLight: '#4268b1',
+    widgetBgDark: '#3d61a3',
+  },
+  {
+    title: 'Pink',
+    navbarBg: '#eb4c89',
+    heroBg: '#fb5092',
+    headerBg: '#d04176',
+    widgetBgLight: '#db4680',
+    widgetBgDark: '#cf4277',
+  },
+]
