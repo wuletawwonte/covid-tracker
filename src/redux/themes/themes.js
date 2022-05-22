1221c35b944e86486fa6cbe7a8b281e7a0933f33
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-22 22:37:00 +0300

    Add themes reducer function

--- src/redux/themes/themes.js
+++ src/redux/themes/themes.js
@@ -7,3 +7,14 @@ const CHANGE_THEME = 'covid-tracker/CHANGE_THEME';
 // Action creators
 export const getTheme = () => ({type: GET_THEME});
 export const changeTheme = (theme) => ({ type: CHANGE_THEME, payload: theme });
+
+export default function themes(state = {theme: themes[0]}, action) {
+  switch(action.type) {
+    case GET_THEME: 
+      return state;
+    case CHANGE_THEME:
+      return { theme: themes.find(theme => theme.title === action.payload)};
+    default:
+      return state;
+  }
+}
