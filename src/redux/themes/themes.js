4ae4e7d3a0c843b925070af091ce3b21fece516f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-22 22:32:30 +0300

    Add theme action creators for both theme actions

--- src/redux/themes/themes.js
+++ src/redux/themes/themes.js
@@ -3,3 +3,7 @@ import themes from './initial-theme-data';
 // Actions
 const GET_THEME = 'covid-tracker/GET_THEME';
 const CHANGE_THEME = 'covid-tracker/CHANGE_THEME';
+
+// Action creators
+export const getTheme = () => ({type: GET_THEME});
+export const changeTheme = (theme) => ({ type: CHANGE_THEME, payload: theme });
